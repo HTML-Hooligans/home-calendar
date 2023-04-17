08284c861885ec704d63c4c6fb661f7272524515
kj-ninja <jakubiak.krzy@gmail.com>
2023-04-17 09:30:13 +0200

    adding routing to app with basic routes like root and auth, removing chakra ui and take mui in its place, adding navbar - draft todo: add craco and setup emotion lib

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Navbar from './components/Navbar/Navbar';
 import './App.css';
+import Login from './pages/auth/Login';
+import NotFound from './pages/404/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth/login" element={<Login />} />
+          <Route path="/auth/sign-up" element={<div>register</div>} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ client/src/pages/auth/Login.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from 'react';
+
+function Login(): ReactElement {
+  return <div>Login component</div>;
+}
+
+export default Login;
