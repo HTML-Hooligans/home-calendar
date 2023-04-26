57b4c818dfdc2c1cf79b912014ba0539c34cfce1
kj-ninja <jakubiak.krzy@gmail.com>
2023-04-26 17:16:23 +0200

    craco file edit

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,21 +1,12 @@
 /* eslint-disable */
-const path = require('path');
-const fs = require('fs');
 const cracoBabelLoader = require('craco-babel-loader');
-
-const appDirectory = fs.realpathSync(process.cwd());
-const resolvePackage = (relativePath) => path.resolve(appDirectory, relativePath);
 
 module.exports = {
   plugins: [
     {
       plugin: cracoBabelLoader,
       options: {
-        includes: [
-          resolvePackage('../packages/ui'),
-          resolvePackage('../packages/utils'),
-          resolvePackage('../packages/types'),
-        ],
+        includes: [],
       },
     },
   ],
