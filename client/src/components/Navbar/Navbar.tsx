b89cffa1c3c313362531f54aba4319d0de213877
kj-ninja <jakubiak.krzy@gmail.com>
2023-04-29 11:44:49 +0200

    adding nav items toggling based on user auth

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -10,6 +10,8 @@ import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
 import * as styles from './Navbar.styles';
 import CustomDrawer from './Drawer';
+import { useUser } from '../../hooks/useUser';
+import { firebase } from '../../services/firebase';
 
 interface Props {
   window?: () => Window;
@@ -17,7 +19,9 @@ interface Props {
 
 export default function Navbar({ window }: Props) {
   const [isMobileOpen, setIsMobileOpen] = useState(false);
+  const { isLoggedIn } = useUser();
   const navigate = useNavigate();
+
   const container = window !== undefined ? () => window().document.body : undefined;
 
   const navItems = [
@@ -33,6 +37,11 @@ export default function Navbar({ window }: Props) {
 
   const handleDrawerToggle = () => {
     setIsMobileOpen((prevState) => !prevState);
+  };
+
+  const handleLogout = async () => {
+    await firebase.auth.signOut();
+    // todo handle logout action?
   };
 
   return (
@@ -59,13 +68,21 @@ export default function Navbar({ window }: Props) {
               LOGO
             </div>
           </Typography>
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-            {navItems.map(({ name, callback }) => (
-              <Button key={name} sx={{ color: '#fff' }} onClick={callback}>
-                {name}
+          {!isLoggedIn ? (
+            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+              {navItems.map(({ name, callback }) => (
+                <Button key={name} sx={{ color: '#fff' }} onClick={callback}>
+                  {name}
+                </Button>
+              ))}
+            </Box>
+          ) : (
+            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+              <Button sx={{ color: '#fff' }} onClick={() => handleLogout()}>
+                Logout
               </Button>
-            ))}
-          </Box>
+            </Box>
+          )}
         </Toolbar>
       </AppBar>
       <Box component="nav">
