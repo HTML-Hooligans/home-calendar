0b6a290e81123e332ca70668125345dcaa05f84e
kj-ninja <jakubiak.krzy@gmail.com>
2023-05-06 19:59:54 +0200

    adding global styles also prepare some theme styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Navbar from './components/Navbar/Navbar';
-import './App.css';
 import Login from './pages/auth/Login';
 import NotFound from './pages/404/NotFound';
 import Register from './pages/auth/Register';
@@ -10,10 +9,13 @@ import { store } from './store/store';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Global } from '@emotion/react';
+import { globalCss } from './theme/theme';
 
 function App() {
   return (
     <>
+      <Global styles={globalCss} />
       <ToastContainer
         position="top-right"
         autoClose={5000}
